f84deca9a9add0291f83e73cc871585cd6261938
Marcos Cordeiro <marcosc974@gmail.com>
2026-06-12 08:44:08 -0300

    fix: new version

--- wp-content/plugins/album-copa-2026/album-copa-2026.php
+++ wp-content/plugins/album-copa-2026/album-copa-2026.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Album Copa 2026
  * Description: Gerencia o envio, aprovação e exibição de figurinhas para o álbum da Copa 2026.
- * Version: 1.1.2
+ * Version: 1.1.3
  * Author: Marcos Cordeiro
  * Text Domain: album-copa-2026
  * License: GPLv2 or later
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
 
-define( 'ALBUM_COPA_2026_VERSION', '1.1.2' );
+define( 'ALBUM_COPA_2026_VERSION', '1.1.3' );
 define( 'ALBUM_COPA_2026_DIR', plugin_dir_path( __FILE__ ) );
 define( 'ALBUM_COPA_2026_URL', plugin_dir_url( __FILE__ ) );
 
