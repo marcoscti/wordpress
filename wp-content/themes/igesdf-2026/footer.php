5078f445b3ab95fd6fa5e46b65020650f72ba3e0
Marcos Cordeiro <marcosc974@gmail.com>
2026-07-02 14:02:33 -0300

    feat: adicionando possibilidade de menu dentro do footer

--- wp-content/themes/igesdf-2026/footer.php
+++ wp-content/themes/igesdf-2026/footer.php
@@ -7,18 +7,24 @@
         </div>
     </div>
     <div class="container">
-        <p class="m-0 text-center text-white">
+        <?php
+        wp_nav_menu([
+            'theme_location' => 'footer_menu',
+            'container'      => false,
+            'menu_class'     => 'navbar-nav',
+            'fallback_cb'    => '__return_false',
+        ]);
+        ?>
+        <p class="m-0 text-center text-white line lh-base">
             Siga nossas redes:<br>
         <ul class="d-flex justify-content-center list-unstyled mb-0 gap-2">
-                <li><a href="https://www.facebook.com/igesdfoficial" target="_blank"><img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.png" alt="Facebook" class="lp-copa-2026-image"></a></li>
-                <li><a href="https://www.instagram.com/iges_df/" target="_blank"><img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram.png" alt="Instagram" class="lp-copa-2026-image"></a></li>
-                <li><a href="https://www.youtube.com//@IGESDFoficial" target="_blank"><img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/youtube.png" alt="YouTube" class="lp-copa-2026-image"></a></li>
-                <li><a href="https://www.linkedin.com/company/igesdf/" target="_blank"><img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/linkedin.png" alt="LinkedIn" class="lp-copa-2026-image"></a></li>
-            
+            <li><a href="https://www.facebook.com/igesdfoficial" target="_blank"><img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.png" alt="Facebook" class="lp-copa-2026-image"></a></li>
+            <li><a href="https://www.instagram.com/iges_df/" target="_blank"><img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram.png" alt="Instagram" class="lp-copa-2026-image"></a></li>
+            <li><a href="https://www.youtube.com//@IGESDFoficial" target="_blank"><img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/youtube.png" alt="YouTube" class="lp-copa-2026-image"></a></li>
+            <li><a href="https://www.linkedin.com/company/igesdf/" target="_blank"><img width="30" src="<?php echo get_template_directory_uri(); ?>/assets/images/linkedin.png" alt="LinkedIn" class="lp-copa-2026-image"></a></li>
         </ul>
         </p>
-        <p class="m-0 text-center text-white">Instituto de Gestão Estratégica de Saúde do Distrito Federal</p>
-        <p class="m-0 text-center text-white">&copy; <?php echo date('Y'); ?> IgesDF. Todos os direitos reservados.</p>
+        <p class="m-0 text-center text-white">Instituto de Gestão Estratégica de Saúde do Distrito Federal IgesDF. <br>Todos os direitos reservados.</p>
     </div>
 </footer>
 <?php wp_footer(); ?>
