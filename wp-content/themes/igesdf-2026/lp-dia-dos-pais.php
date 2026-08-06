e29d2218f8a55cd0b5db97b58a2b5bd753a4fea3
Marcos Cordeiro <marcosc974@gmail.com>
2026-08-06 08:39:23 -0300

    fix: novas imagens

--- wp-content/themes/igesdf-2026/lp-dia-dos-pais.php
+++ wp-content/themes/igesdf-2026/lp-dia-dos-pais.php
@@ -7,7 +7,7 @@ get_header();
 
 <section class="lp-hero">
     <picture>
-        <source media="(max-width: 768px)" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/hero-mobile.png" style="width: 100%; height: auto;">
+        <source media="(max-width: 768px)" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/hero-mobile.jpeg" style="width: 100%; height: auto;">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero.png" style="width: 100%; height: auto;" alt="Dia dos Pais" class="img-fluid">
     </picture>
 </section>
