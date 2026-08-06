660e5ed94d85fecbddecd2a2a0396616e1abb4b0
Marcos Cordeiro <marcosc974@gmail.com>
2026-08-06 14:11:04 -0300

    fix: ajuste de responsividade

--- wp-content/plugins/homenagem-pais/homenagem-pais.php
+++ wp-content/plugins/homenagem-pais/homenagem-pais.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Homenagem Pais
  * Description: Registra o Custom Post Type `homenagem` e provê endpoint AJAX para submissão de homenagens (para uso em landing pages).
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Marcos Cordeiro
  */
 
